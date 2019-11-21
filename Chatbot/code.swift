8346651dbdb53f2db103bf684b39d5da41e3a721
lizeth9797 <36308437+lizeth9797@users.noreply.github.com>
2019-11-21 09:27:37 -0600

    Update code.swift

--- Chatbot/code.swift
+++ Chatbot/code.swift
@@ -28,11 +28,44 @@ class ViewController: UIViewController {
             let response = response as! AIResponse
             if let textResponse = response.result.fulfillment.speech {
                 self.speechAndText(text: textResponse)
-                if textResponse.hasSuffix("karina"){
-                    self.imagen.image=UIImage(named: "rectangulo")
-                } else if textResponse.hasSuffix("rulo"){
-                    self.imagen.image=UIImage(named: "triangulo")
+                
+                //AQUI EMPIEZAN LAS IMÁGENES
+                
+                if textResponse.hasSuffix("Dirigete hacia el edificio de Posgrados y lo encontrarás a un costado de la escultura amarilla"){
+                    self.imagen.image=UIImage(named: "auditorio")
+                } else if textResponse.hasSuffix("Está a un costado del edificio de extensión cultural, el cual está frente a los edificios de idiomas."){
+                    self.imagen.image=UIImage(named: "barros")
+                }else if textResponse.hasSuffix("rulo"){
+                    self.imagen.image=UIImage(named: "En cada edificio de la facultad hay baños, dirígete al más cercano")
+                }else if textResponse.hasSuffix("La biblioteca se encuentra a un costado de las canchas de fútbol, dirígete hacia las canchas ubicadas cerca de la entrada principal y encontrarás la biblioteca."){
+                    self.imagen.image=UIImage(named: "biblioteca")
+                }else if textResponse.hasSuffix("La cafetería se encuentra a un costado de las cachas, puedes llegar por la entrada principal o por la entrada de idiomas. ¿Gustas saber el menú de hoy?"){
+                    self.imagen.image=UIImage(named: "cafeteria")
+                }else if textResponse.hasSuffix("Cedetec está a un costado del estacionamiento de la entrada de la MEGA."){
+                    self.imagen.image=UIImage(named: "cedetec")
+                }else if textResponse.hasSuffix("Coesi esta en la planta baja del edificio A8, el cual se encuentra a un costado de las canchas de fútbol."){
+                    self.imagen.image=UIImage(named: "coesi")
+                }else if textResponse.hasSuffix("Se encuentra ubicado en la planta baja del edificio A15. Puedes llegar por la entrada principal y lo encontrarás del lado derecho"){
+                    self.imagen.image=UIImage(named: "cedam")
+                }else if textResponse.hasSuffix("Puedes llegar por la entrada de Idiomas, acercarte al Teatro Barros Sierra y encontrarás idiomas frente al centro cultural."){
+                    self.imagen.image=UIImage(named: "idiomas")
                 }
+                
+                //IMAGENES DE "NO ENTIENDO"
+
+                else if textResponse.hasSuffix("Ups, no he entendido a que te refieres."){
+                    self.imagen.image=UIImage(named: "goyo1")
+                }else if textResponse.hasSuffix("¿Podrías repetirlo, por favor?"){
+                    self.imagen.image=UIImage(named: "goyo1")
+                }else if textResponse.hasSuffix("¿Disculpa?"){
+                    self.imagen.image=UIImage(named: "goyo1")
+                }else if textResponse.hasSuffix("¿Cómo?"){
+                    self.imagen.image=UIImage(named: "goyo1")
+                }
+                
+                
+                
+                
             }
         }, failure: { (request, error) in
             print(error!)
